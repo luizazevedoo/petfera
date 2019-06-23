f137fd4eed62e3b42797eebc1042ebd2a2fae42b
acsa-laa <44912056+acsa-laa@users.noreply.github.com>
2019-06-23 13:25:03 -0300

    Update sistema.h

--- sistema.h
+++ sistema.h
@@ -2,6 +2,8 @@
 #define SISTEMA_H
 
 /**@brief classe referente à manipulação de informações, cadastro e venda de animais*/
+#include <map>
+
 class Sistema{
 private:
 	/**@brief Guarda a seleção do menu*/
@@ -19,7 +21,18 @@ private:
 		Veterinario _veterinario;
 		Tratador _tratador;
 	} animal;
-	
+	int posicaoAnf;
+	int posicaoAve;
+	int posicaoMam;
+	int posicaoRep;
+	int posicaoTrat;
+	int posicaoVet;
+	map<int, Tratador> mapTratador;
+	map<int, Veterinario> mapVeterinario;
+	map<int, Mamifero> mapMamifero;
+	map<int, Ave> mapAve;
+	map<int, Reptil> mapReptil;
+	map<int, Anfibio> mapAnfibio;	
 public:
 	Sistema();
 	void displayMenu();
