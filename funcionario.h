cd01ab2f7148088f89a764a9a2717a9276fcbb35
acsa-laa <44912056+acsa-laa@users.noreply.github.com>
2019-06-23 20:59:54 -0300

    Update funcionario.h

--- funcionario.h
+++ funcionario.h
@@ -13,9 +13,22 @@ class Funcionario
 		string especialidade;
 	public:
 		virtual void consultar_funcionario() = 0;
-		Funcionario();
+		Funcionario(int id, string nome, string cpf, short idade, short tipo_sanguineo, char fator_rh, string especialidade);
 		~Funcionario();
-		
+		void setId(int id);
+		int getId();
+		void setNome(string nome);
+		string getNome();
+		void setCpf(string cpf);
+		string getCpf();
+		void setIdade(short idade);
+		short getIdade();
+		void setTipo_sanguineo(short tipo_sanguineo);
+		short getTipo_sanguineo();
+		void setFator_rh(char fator_rh);
+		char getFator_rh();
+		void setEspecialidade(string especialidade);
+		string getEspecialidade();
 };
 
 #endif
