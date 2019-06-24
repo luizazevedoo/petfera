1b7e69d818e7c63901a49aec01ac8579be5b6551
luizazevedoo <47855767+luizazevedoo@users.noreply.github.com>
2019-06-24 16:31:18 -0300

    Update anfibio.cpp

--- anfibio.cpp
+++ anfibio.cpp
@@ -6,10 +6,8 @@
 
 using namespace std;
 
-Anfibio::Anfibio(Veterinario veterinario, Tratador tratador, int id, string classe, string nome_cientifico, char sexo, double tamanho, string dieta, string nome_batismo, int total_mudas, string ultima_muda): Animal(veterinario, tratador, id, classe, nome_cientifico, sexo, tamanho, dieta, nome_batismo){
-	this->total_mudas = total_mudas;
-	this->ultima_muda = ultima_muda;
-};
+Anfibio::Anfibio():Animal(){
+}
 
 void Anfibio::setTotal_mudas(int total_mudas){
 	this->total_mudas = total_mudas;
