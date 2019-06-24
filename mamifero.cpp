cfe73fb9d4a9713501f212fafd008e5738c1640b
luizazevedoo <47855767+luizazevedoo@users.noreply.github.com>
2019-06-24 16:31:27 -0300

    Update mamifero.cpp

--- mamifero.cpp
+++ mamifero.cpp
@@ -2,8 +2,7 @@
 
 using namespace std;
 
-Mamifero::Mamifero(Veterinario veterinario, Tratador tratador, int id, string classe, string nome_cientifico, char sexo, double tamanho, string dieta, string nome_batismo, string cor_pelo): Animal(veterinario, tratador, id, classe, nome_cientifico, sexo, tamanho, dieta, nome_batismo){
-	this->cor_pelo = cor_pelo;
+Mamifero::Mamifero():Animal(){
 };
 
 void Mamifero::setCor_pelo(string cor_pelo){
