278781a71c83cb811e17d5bae1562095fda5f6a2
acsa-laa <44912056+acsa-laa@users.noreply.github.com>
2019-06-23 21:15:09 -0300

    Update sistema.h

--- sistema.h
+++ sistema.h
@@ -3,24 +3,19 @@
 
 /**@brief classe referente à manipulação de informações, cadastro e venda de animais*/
 #include <map>
+#include "animal.h"
+#include "ave.h"
+#include "anfibio.h"
+#include "reptil.h"
+#include "mamifero.h"
+#include "manipular_arquivos.h"
 
 class Sistema{
 private:
 	/**@brief Guarda a seleção do menu*/
 	int selecaoAtual;
 	int selecaoClasse;
-	struct Animal{
-		int _id;
-		double _tamanho;
-		string _classe;
-		string _nome_cientifico;
-		string _nome_batismo;
-		string _dieta;
-		char _sexo;
-		bool _ameacadoExtincao;
-		Veterinario _veterinario;
-		Tratador _tratador;
-	} animal;
+	int selecaoFuncionarios;
 	int posicaoAnf;
 	int posicaoAve;
 	int posicaoMam;
